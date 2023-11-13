7e5ed17449972d8a4394f744681e6f2ad4747aff
AshinrajM <ashinrajeevank@outlook.com>
2023-11-13 15:45:06 +0530

    Navbar related items added and unwanted items cleared

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -14,7 +13,7 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
+        > 
           Learn React
         </a>
       </header>
